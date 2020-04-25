5439825e744cd1b37db77b95468b5b6bb1420c93
Bayu Trisnapati <bayutrisnapati@gmail.com>
2020-04-26 02:32:27 +0800

    add alert

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ send_email = () => {
   const email = document.getElementById("email").value;
   const pesan = document.getElementById("pesan").value;
   if(nama === '') {
-    alert("Nama wajib diisi");
+    document.getElementById("alert_kontak").style.backgroundColor="#e74c3c";
+    document.getElementById("alert_kontak").style.display='block';
+    document.getElementById("alert_status").innerHTML = "Nama wajib diisi";
   } else if(email === '') {
-    alert("Alamat email wajib diisi");
+    document.getElementById("alert_kontak").style.backgroundColor="#e74c3c";
+    document.getElementById("alert_kontak").style.display='block';
+    document.getElementById("alert_status").innerHTML = "Alamat email wajib diisi";
   } else if(pesan === '') {
-    alert("Pesan wajib diisi");
+    document.getElementById("alert_kontak").style.backgroundColor="#e74c3c";
+    document.getElementById("alert_kontak").style.display='block';
+    document.getElementById("alert_status").innerHTML = "Pesan wajib diisi";
   } else {
-    alert( `Terimakasih ${nama} telah menghubungi kami`);
+    document.getElementById("alert_kontak").style.backgroundColor="#2ecc71";
+    document.getElementById("alert_kontak").style.display='block';
+    document.getElementById("alert_status").innerHTML = `Terimakasih ${nama} telah menghubungi kami`;
   }
 
 }
