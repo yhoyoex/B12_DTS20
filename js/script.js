9eb105c273db3441f2a2496fff364121f47fadf5
Bayu Trisnapati <bayutrisnapati@gmail.com>
2020-04-25 23:34:11 +0800

    update form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 send_email = () => {
-  alert("Pesan dikirim");
+  const nama = document.getElementById("nama").value;
+  const email = document.getElementById("email").value;
+  const pesan = document.getElementById("pesan").value;
+  if(nama === '') {
+    alert("Nama wajib diisi");
+  } else if(email === '') {
+    alert("Alamat email wajib diisi");
+  } else if(pesan === '') {
+    alert("Pesan wajib diisi");
+  } else {
+    alert( `Terimakasih ${nama} telah menghubungi kami`);
+  }
+
 }
